e32b0cff823020677f4cabe80b7104f286fc4c32
Pavel Surmenok <surmenok@gmail.com>
2015-05-04 19:25:03 -0700

    Close file streams in MNISTReader

--- src/main/java/com/surmenok/pavel/mnist/MNISTReader.java
+++ src/main/java/com/surmenok/pavel/mnist/MNISTReader.java
@@ -6,55 +6,56 @@ import java.io.IOException;
 
 public class MNISTReader {
     public double[][] readImages(String fileName) throws IOException, InvalidFileFormatException {
-        DataInputStream stream = new DataInputStream(new FileInputStream(fileName));
-
-        int magicNumber = stream.readInt();
-        if (magicNumber != 2051) {
-            throw new InvalidFileFormatException("Wrong magic number: " + magicNumber + "; expected: 2051");
-        }
-
-        int imageCount = stream.readInt();
-        int rowCount = stream.readInt();
-        int columnCount = stream.readInt();
-
-        double[][] allImages = new double[imageCount][rowCount * columnCount];
-
-        for (int i = 0; i < imageCount; i++) {
-            if(stream.available() <= 0) {
-                throw new InvalidFileFormatException("The file contains less than " + imageCount + " images");
+        try(DataInputStream stream = new DataInputStream(new FileInputStream(fileName))) {
+            int magicNumber = stream.readInt();
+            if (magicNumber != 2051) {
+                throw new InvalidFileFormatException("Wrong magic number: " + magicNumber + "; expected: 2051");
             }
 
-            for (int column = 0; column < columnCount; column++) {
-                for (int row = 0; row < rowCount; row++) {
-                    allImages[i][column * rowCount + row] = stream.readUnsignedByte();
+            int imageCount = stream.readInt();
+            int rowCount = stream.readInt();
+            int columnCount = stream.readInt();
+
+            double[][] allImages = new double[imageCount][rowCount * columnCount];
+
+            for (int i = 0; i < imageCount; i++) {
+                if (stream.available() <= 0) {
+                    throw new InvalidFileFormatException("The file contains less than " + imageCount + " images");
+                }
+
+                for (int column = 0; column < columnCount; column++) {
+                    for (int row = 0; row < rowCount; row++) {
+                        allImages[i][column * rowCount + row] = stream.readUnsignedByte();
+                    }
                 }
             }
-        }
 
-        return allImages;
+            return allImages;
+        }
     }
 
     public double[][] readLabels(String fileName) throws IOException, InvalidFileFormatException {
-        DataInputStream stream = new DataInputStream(new FileInputStream(fileName));
+        try(DataInputStream stream = new DataInputStream(new FileInputStream(fileName))) {
 
-        int magicNumber = stream.readInt();
-        if (magicNumber != 2049) {
-            throw new InvalidFileFormatException("Wrong magic number: " + magicNumber + "; expected: 2049");
-        }
-
-        int labelCount = stream.readInt();
-
-        double[][] allLabels = new double[labelCount][10];
-
-        for (int i = 0; i < labelCount; i++) {
-            if(stream.available() <= 0) {
-                throw new InvalidFileFormatException("The file contains less than " + labelCount + " images");
+            int magicNumber = stream.readInt();
+            if (magicNumber != 2049) {
+                throw new InvalidFileFormatException("Wrong magic number: " + magicNumber + "; expected: 2049");
             }
 
-            byte label = stream.readByte();
-            allLabels[i][label % 10] = 1;
-        }
+            int labelCount = stream.readInt();
 
-        return  allLabels;
+            double[][] allLabels = new double[labelCount][10];
+
+            for (int i = 0; i < labelCount; i++) {
+                if (stream.available() <= 0) {
+                    throw new InvalidFileFormatException("The file contains less than " + labelCount + " images");
+                }
+
+                byte label = stream.readByte();
+                allLabels[i][label % 10] = 1;
+            }
+
+            return allLabels;
+        }
     }
 }
